a2c680473c33133310a409e7e427fcb6cab1b053
chisom ogbodo <ogbodochisom37@gmail.com>
2025-11-26 11:25:02 +0100

    Added docstring to the functions

--- core/utils.py
+++ core/utils.py
@@ -1,11 +1,13 @@
-import jwt
+
 from pwdlib import PasswordHash
 
 password_hash = PasswordHash.recommended()
 
 def hash_password(passwd: str | bytes) -> str:
+    """Hash password with the recommended algorithm"""
     return password_hash.hash(passwd)
 
 def verify_password(plain: str | bytes, hashed: str | bytes) -> bool:
+    """Verifies if the hashed password is equal to the plain password"""
     return password_hash.verify(plain, hashed)
 
