085e642a4474d0c8e769c1694d4a445d9b33fb80
chisom ogbodo <ogbodochisom37@gmail.com>
2025-11-26 15:37:55 +0100

    Add delete_user crud function

--- crud/user.py
+++ crud/user.py
@@ -1,13 +1,14 @@
 
+from fastapi import Depends
 from psycopg import Connection
 from pydantic import EmailStr
 
-from notes_api.core.utils import hash_password
+from notes_api.core import utils 
 from notes_api.schemas.notes import User
 
 async def add_user(username: str, email: EmailStr, password: str | bytes, conn: Connection):
     sql = """INSERT INTO users(username, email, password) VALUES (%s, %s, %s);"""
-    hashed_password = hash_password(password)
+    hashed_password = utils.hash_password(password)
     with conn.cursor() as cur:
         cur.execute(sql, (username, email, hashed_password))
 
@@ -19,12 +20,16 @@ async def get_user(email: str, conn: Connection):
     
 async def update_user(user_id: int, user: User, conn: Connection):
     sql = """UPDATE users SET username = %s, email = %s, password = %s WHERE user_id = %s RETURNING *;"""
+    
     with conn.cursor() as cur:
-        cur.execute(sql, {'user_id': user_id, **user.model_dump()})
+        data = user.model_dump()
+        print(data)
+        print(data.update({'user_id': user_id}))
+        cur.execute(sql, (user.username, user.email, utils.hash_password(user.password), user_id,))
         return cur.fetchone()
     
-#async def delete_user(user_id, conn: Connection):
- #   sql = """DELETE FROM users WHERE user_id = %(user_id)s; RETURNING *"""
-  #  with conn.cursor() as cur:
-   #     cur.execute(sql, {'user_id': user_id})
-    #    return cur.fetchone()
+async def delete_user(user_id, conn: Connection):
+    sql = """DELETE FROM users WHERE user_id = %(user_id)s; RETURNING *"""
+    with conn.cursor() as cur:
+        cur.execute(sql, {'user_id': user_id})
+        return cur.fetchone()
