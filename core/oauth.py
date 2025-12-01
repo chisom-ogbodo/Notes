b8b746467023672fd5b5384f81c2ff64c9c603db
chisom ogbodo <ogbodochisom37@gmail.com>
2025-12-01 17:53:04 +0100

    updated the sub jwt key from a tuple to dict

--- core/oauth.py
+++ core/oauth.py
@@ -10,7 +10,7 @@ from jwt.exceptions import InvalidTokenError
 from notes_api.core import utils, config
 from notes_api.crud.user import get_user
 from notes_api.models.db import get_conn
-from notes_api.schemas.notes import Token
+from notes_api.schemas import Token
 
 settings = config.settings
 oauth_scheme = OAuth2PasswordBearer(tokenUrl='/login')
@@ -53,7 +53,7 @@ async def login_user(
     user = await get_user(email, conn)
     if not user: 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Invalid username or pasword')
-    if not utils.verify_password(flow.password, user[3]):
+    if not utils.verify_password(flow.password, user.get('password')):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid email or password')
-    access_token = create_access_token({'sub': str(user[0])})
+    access_token = create_access_token({'sub': str(user.get('user_id'))})
     return Token(access_token=access_token, token_type='Bearer')
