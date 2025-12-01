cea41ae8e441bdea17284d42931d382ddb69a5db
chisom ogbodo <ogbodochisom37@gmail.com>
2025-12-01 18:01:55 +0100

    Refactor the pydantic models for use as fastapi response models

--- schemas.py
+++ schemas.py
@@ -4,24 +4,30 @@ from typing import Annotated
 from pydantic import BaseModel, EmailStr, Field
 
 class Notes(BaseModel):
-  
+    note_id: int
+    user_id: int
+    title: Annotated[str | None, Field(max_length=57)] = None
+    content: str
+    created_date: datetime
+
+class NotesUpdate(BaseModel):
     title: Annotated[str | None, Field(max_length=57)] = None
     content: str
 
-class NotesUpdate(BaseModel):
-    title: str | None = None
-    content: str
+class NotesCreate(NotesUpdate):
+    pass
 
-class User(BaseModel):
-    username: str | None = None
-    email: EmailStr
-    password: str
+class UserBase(BaseModel):
+    username: Annotated[str | None, Field(max_length=50)] = None
+    email: Annotated[EmailStr, Field(max_length=50)]
+    
+class UserOut(UserBase):
+    pass
 
-class UserOut(User):
-    user_id: int
-   
+class UserCreate(UserBase):
+    password: Annotated[str, Field(max_length=255)]
 
-class UserInDb(UserOut, User):
+class UserUpdate(UserCreate):
     pass
 
 class Token(BaseModel):
