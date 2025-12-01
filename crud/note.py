b50823e009aa905fac428b65af9ee4a9bf7dfa95
chisom ogbodo <ogbodochisom37@gmail.com>
2025-12-01 17:55:52 +0100

    Replaced the default row_factory to dict_row

--- crud/note.py
+++ crud/note.py
@@ -1,28 +1,28 @@
 
 from psycopg import Connection
+from psycopg.rows import dict_row
 
-from notes_api.schemas.notes import Notes, NotesUpdate
+from notes_api.schemas import Notes, NotesCreate, NotesUpdate
     
 async def crud_retrieve(user_id: int, conn: Connection, skip: int = 0, limit: int = 10):
     sql = f"""SELECT * FROM notes
     WHERE user_id = %s ORDER BY note_id 
     OFFSET {skip} ROWS FETCH FIRST {limit} ROWS ONLY;"""
-    with conn.cursor() as cur:
+    with conn.cursor(row_factory=dict_row) as cur:
         cur.execute(sql, (user_id,))
         res = cur.fetchall()
         return res
     
-async def crud_create(user_id: int, note: Notes, conn: Connection):
+async def crud_create(user_id: int, note: NotesCreate, conn: Connection):
     sql = """INSERT INTO notes (user_id, title, content) VALUES (%s, %s, %s) RETURNING *"""
-    with conn.cursor() as cur:
+    with conn.cursor(row_factory=dict_row) as cur:
         if not note.title: 
            note.title = note.content[:57]
-           print(note.title)
         cur.execute(sql, (user_id, note.title, note.content,))
         
 async def crud_update(user_id: int, note_id, note: NotesUpdate, conn: Connection):
     sql = """UPDATE notes SET content = %s WHERE user_id = %s AND note_id = %s RETURNING *;""" 
-    with conn.cursor() as cur:
+    with conn.cursor(row_factory=dict_row) as cur:
         if note.title:
             sql = """UPDATE notes SET title = %s, content = %s WHERE user_id = %s AND note_id = %s RETURNING *;"""       
             cur.execute(sql, (note.title, note.content, user_id, note_id,))
@@ -32,7 +32,7 @@ async def crud_update(user_id: int, note_id, note: NotesUpdate, conn: Connection
        
 async def crud_delete(user_id: int, note_id: int, conn: Connection):
     sql = """DELETE FROM notes WHERE user_id = %s AND note_id = %s RETURNING *;"""
-    with conn.cursor() as cur:
+    with conn.cursor(row_factory=dict_row) as cur:
         cur.execute(sql, (user_id, note_id,))
         data = cur.fetchone()
     return data
--- crud/user.py
+++ crud/user.py
@@ -1,35 +1,36 @@
 
 from fastapi import Depends
 from psycopg import Connection
+from psycopg.rows import dict_row
 from pydantic import EmailStr
 
 from notes_api.core import utils 
-from notes_api.schemas.notes import User
+from notes_api.schemas import UserUpdate
 
 async def add_user(username: str, email: EmailStr, password: str | bytes, conn: Connection):
-    sql = """INSERT INTO users(username, email, password) VALUES (%s, %s, %s);"""
+    sql = """INSERT INTO users(username, email, password) VALUES (%s, %s, %s) RETURNING *;"""
     hashed_password = utils.hash_password(password)
-    with conn.cursor() as cur:
+    with conn.cursor(row_factory=dict_row) as cur:
         cur.execute(sql, (username, email, hashed_password))
+        return cur.fetchone()
 
 async def get_user(email: str, conn: Connection):
     sql = """SELECT * FROM users WHERE email = %(email)s;"""
-    with conn.cursor() as cur:
+    with conn.cursor(row_factory=dict_row) as cur:
         cur.execute(sql, {'email': email})
         return cur.fetchone()
     
-async def update_user(user_id: int, user: User, conn: Connection):
+async def update_user(user_id: int, user: UserUpdate, conn: Connection):
     sql = """UPDATE users SET username = %s, email = %s, password = %s WHERE user_id = %s RETURNING *;"""
     
-    with conn.cursor() as cur:
+    with conn.cursor(row_factory=dict_row) as cur:
         data = user.model_dump()
-        print(data)
         print(data.update({'user_id': user_id}))
         cur.execute(sql, (user.username, user.email, utils.hash_password(user.password), user_id,))
         return cur.fetchone()
     
 async def delete_user(user_id, conn: Connection):
-    sql = """DELETE FROM users WHERE user_id = %(user_id)s; RETURNING *"""
-    with conn.cursor() as cur:
+    sql = """DELETE FROM users WHERE user_id = %(user_id)s RETURNING *;"""
+    with conn.cursor(row_factory=dict_row) as cur:
         cur.execute(sql, {'user_id': user_id})
         return cur.fetchone()
